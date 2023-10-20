577a2336c949fe699f2a9e131e7dd3af5009c808
wilsondefreitas-dev <wilsondefreitas92@gmail.com>
2023-10-20 20:56:59 -0300

    :sparkles: feat: Added mysql into express server

--- node/index.js
+++ node/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const mysql = require('mysql');
+
 const app = express();
 const port = 3030;
+
+const connection = mysql.createConnection({
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb',
+});
+
+const sql = `INSERT INTO names(name) values('Wilson')`;
+connection.query(sql);
+connection.end();
 
 app.get("/", (req, res) => {
     res.send("<h1>hi, I'm live updating!</h1>");
