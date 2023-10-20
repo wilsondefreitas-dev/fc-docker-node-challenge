57dfd6587dd70a81d492bdcb56ce4f22d2d72962
wilsondefreitas-dev <wilsondefreitas92@gmail.com>
2023-10-20 18:56:54 -0300

    :sparkles: feat: Updated index.js in node

--- node/index.js
+++ node/index.js
@@ -2,5 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3030;
 
+app.get("/", (req, res) => {
+    res.send("<h1>hi!</h1>");
+});
+
 app.listen(port, () => console.log(`Server running on ${port}`));
 
