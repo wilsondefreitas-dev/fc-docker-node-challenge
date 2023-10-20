cf9788569e74b98753431d9ca48373abf161fee6
wilsondefreitas-dev <wilsondefreitas92@gmail.com>
2023-10-20 20:07:23 -0300

    :bricks: ci: Created docker-compose.yaml file

--- node/index.js
+++ node/index.js
@@ -3,8 +3,7 @@ const app = express();
 const port = 3030;
 
 app.get("/", (req, res) => {
-    res.send("<h1>hi!</h1>");
+    res.send("<h1>hi, I'm live updating!</h1>");
 });
 
 app.listen(port, () => console.log(`Server running on ${port}`));
-
