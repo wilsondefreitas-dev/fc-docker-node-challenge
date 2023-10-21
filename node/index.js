a5b6c63f4a982eba74822051ffb1dc4956545eac
wilsondefreitas-dev <wilsondefreitas92@gmail.com>
2023-10-20 21:15:38 -0300

    :sparkles: feat: Added script to save and read the database in express server

--- node/index.js
+++ node/index.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 
 const app = express();
 const port = 3030;
+let names = [];
 
 const connection = mysql.createConnection({
     host: 'db',
@@ -11,12 +12,26 @@ const connection = mysql.createConnection({
     database: 'nodedb',
 });
 
-const sql = `INSERT INTO names(name) values('Wilson')`;
-connection.query(sql);
+const sqlInsertNames = `INSERT INTO names(name) values('Wilson de Freitas Junior')`;
+connection.query(sqlInsertNames);
+
+const sqlGetNames= `SELECT * FROM names`;
+connection.query(sqlGetNames, (error, results) => {
+
+    if (error) return console.error(error.message);
+    names = results;
+
+});
+
 connection.end();
 
 app.get("/", (req, res) => {
-    res.send("<h1>hi, I'm live updating!</h1>");
+    res.send(`<div>
+        <h1>Full Cycle Rocks!</h1>
+        <ul>
+            ${names.map(data => `<li>${data.name}</li>`).join("")}
+        </ul>
+    </div>`);
 });
 
 app.listen(port, () => console.log(`Server running on ${port}`));
